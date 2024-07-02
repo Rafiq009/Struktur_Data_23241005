ae4e777c056858413c8bceb5f32098f3f3e3b0a3
Rafiq009 <145946261+Rafiq009@users.noreply.github.com>
2024-07-01 17:20:33 -0700

    Add files via upload
    pak UAS nya ada di "TUGAS UAS.py"

---
+++ meet.py
@@ -0,0 +1,38 @@
+# test
+
+x = 3
+print("nilai uas saya adalah",x)
+x = 6
+y = 9
+print("nilai uas saya adalah",x)
+print("nilai uas saya adalah",y)
+
+
+x = 1000
+print("nilai uas saya adalah",x)
+
+
+
+x = 3
+print("nilai uas saya adalah",x)
+x = 6
+y = 9
+print("nilai uas saya adalah",x)
+print("nilai uas saya adalah",y)
+
+
+x = 1000
+print("nilai uas saya adalah",x)
+
+
+
+x = 3
+print("nilai uas saya adalah",x)
+x = 6
+y = 9
+print("nilai uas saya adalah",x)
+print("nilai uas saya adalah",y)
+
+
+x = 1000
+print("nilai uas saya adalah",x)
